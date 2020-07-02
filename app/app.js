ab1cabdf67358594b2f864793c517688ee0c6b5c
Imagineee <imagineeelego@gmail.com>
2020-07-02 16:39:45 +0100

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,3 +1,9 @@
+    if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
+
 function save(html,css,js) {
     var Name = document.getElementById("filename").value;
     if(Name == "") {
